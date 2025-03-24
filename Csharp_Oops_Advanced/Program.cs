03a60d534cddabbb1e78827de332470ba636cba5
vishwjeet <vrvishwujgare@gmail.com>
2025-03-24 14:45:03 +0530

    Encapsulation Demo

--- Csharp_Oops_Advanced/Program.cs
+++ Csharp_Oops_Advanced/Program.cs
@@ -1,4 +1,5 @@
 ﻿
+using Encapsulation.Encapsulation;
 using System;
 
 
@@ -8,7 +9,29 @@ namespace Csharp_Oops_Advanced
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Created 4 pillar of OOPS branch");
+            //Console.WriteLine("Created 4 pillar of OOPS branch");
+            //Encapsulation
+
+            try {
+                EncapsulationDemo encap = new EncapsulationDemo();
+                Console.WriteLine($"Bank Name :  {encap.BankName}");
+
+                encap.CustomerName = "Jeet Ujgare";
+                Console.WriteLine($"Customer Name : {encap.CustomerName}");
+
+                encap.Amount = 100.99;
+                Console.WriteLine($"Bank Balance : {encap.Amount}");
+
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+    
+
+            
         }
     }
 }
---
+++ Encapsulation/Encapsulation/EncapsulationDemo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation.Encapsulation
+{
+    public class EncapsulationDemo
+    {
+        private double _Amount;
+        //private string _CustomerName;
+        private readonly string _BankName = "State Bank of India";
+
+        public double Amount
+        {
+            get { 
+                return _Amount;
+            }
+
+            set {
+                if (value < 0) { 
+                    throw new Exception("Please pass Positive Value");
+                } else { 
+                    _Amount = value;
+                }
+            }
+        
+        }
+
+
+        public string BankName
+        {
+
+            get { 
+                return _BankName;
+            }
+        }
+        public string CustomerName { get; set; }
+           
+    }
+}
